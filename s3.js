947da05eed42e69b85c2bced65cc675cf8a57614
zdemopage <129005363+zdemopage@users.noreply.github.com>
2023-03-26 22:43:45 -0300

    Update s3.js

--- s3.js
+++ s3.js
@@ -14,8 +14,7 @@ function uploadObject() {
 
   // Set the S3 bucket params
   const AWS = window.AWS;
-  AWS.config.region = 'eu-west-1';
-  AWS.EventListeners.Core.removeListener('validate', aws.EventListeners.Core.VALIDATE_REGION);
+  AWS.EventListeners.Core.removeListener('validate', AWS.EventListeners.Core.VALIDATE_REGION);
 
   var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
