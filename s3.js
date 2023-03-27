32e7c5208c2bbbc1efeafd4f5af52a5da28b3af1
zdemopage <129005363+zdemopage@users.noreply.github.com>
2023-03-27 00:16:30 -0300

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,72 +1,57 @@
 // Initialize the S3 client
 function uploadObject() {
   // Get the form and debug div elements
-  var form = document.getElementById('upload-form');
-  var debug = document.getElementById('debug');
+  const form = document.getElementById('upload-form');
+  const debug = document.getElementById('debug');
 
   // Get the selected region and bucket name
   const aws_region = document.getElementById('region-select').value;
   const bucketName = document.getElementById('bucket').value;
-
-  // Get the file from the form input  
-  const fileInput = document.getElementById('fileInput');
+  const reader = new FileReader();
+  const fileInput  = document.getElementById('fileInput');
   const file = fileInput.files[0];
-  const fileName = file.name;
-  const bucketUrl = 'https://uber.s3.amazonaws.com/';
-  const fileUrl = bucketUrl + fileName;
-  fetch(fileUrl, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': file.type
-    },
-    body: file
-  })
-  .then(response => {
-    console.log("File uploaded successfully to: ", fileUrl);
-  })
-  .catch(error => {
-    console.log("Error uploading file: ", error);
-  });
-
-
-  // Set the S3 bucket params
-  // const AWS = window.AWS;
-  // AWS.EventListeners.Core.removeListener('validate', AWS.EventListeners.Core.VALIDATE_PARAMETERS);
-  // var s3 = new AWS.S3({
-  //  apiVersion: '2006-03-01',
-  //  signatureVersion: 'v4',
-  //  maxRetries: 15,
-  //  region: aws_region,
-  //  signRequest: false
-  //});
-    
-  //var params = {
-  //  Bucket: bucketName,
-  //  Key: file.name,
-  //  Body: file
-  //}
+  reader.readAsText(file);
+  const fileContents = reader.result;
   
-  //console.log("form: ", form);
-  //console.log("debug: ", debug);
-  //console.log("bucketName: ", bucketName);
-  //console.log("fileInput: ", fileInput);
-  //console.log("file: ", file);
-  //console.log("s3: ", s3);
-  //console.log("params: ", params);
+  // Set the S3 bucket params
+  //const AWS = window.AWS;
+  //AWS.EventListeners.Core.removeListener('validate', AWS.EventListeners.Core.VALIDATE_PARAMETERS);
+  const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    signatureVersion: 'v4',
+    maxRetries: 15,
+    region: aws_region,
+    signRequest: false
+  });
+    
+  const params = {
+    Bucket: bucketName,
+    Key: file.name,
+    ContentType: file.type,
+    Body: fileContents
+  }
+  
+  console.log("form: ", form);
+  console.log("debug: ", debug);
+  console.log("bucketName: ", bucketName);
+  console.log("fileInput: ", fileInput);
+  console.log("file: ", file);
+  console.log("s3: ", s3);
+  console.log("params: ", params);
   
   // Make an unauthenticated request to the S3 bucket
-  //s3.upload(params, function(err, data) {
-  //s3.makeUnauthenticatedRequest('PutObject', params, function(err, data) {
-  //  if (err) {
-  //    debug.innerHTML = `Error sending file`;
-  //    debug.classList.add('has-content');
-  //    console.log(err, err.stack);
-  //  } else {
-  //    debug.innerHTML = `File sent successfully`;
-  //    debug.classList.add('has-content');
-  //    console.log("OK: ", data.Body.toString());
-  //  }
-  //});
+  s3.upload(params, function(err, data) {
+  s3.makeUnauthenticatedRequest('PutObject', params, function(err, data) {
+    if (err) {
+      debug.innerHTML = `Error sending file`;
+      debug.classList.add('has-content');
+      console.log(err, err.stack);
+    } else {
+      debug.innerHTML = `File sent successfully`;
+      debug.classList.add('has-content');
+      console.log("OK: ", data.Body.toString());
+    }
+  });
 
   // Debug functionality
   //s3.on('send', (data) => {
